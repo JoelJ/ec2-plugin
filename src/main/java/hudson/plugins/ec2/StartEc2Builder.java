eb3fe4cff6e77edfa9c5364ea5ebeac8dd389149
joeljohnson <joeljohnson@attask.com>
2012-06-30 20:34:18 -0600

    allow user to create multiple of one ami by using the 'count' tag and specifying a number.

--- src/main/java/hudson/plugins/ec2/StartEc2Builder.java
+++ src/main/java/hudson/plugins/ec2/StartEc2Builder.java
@@ -35,13 +35,17 @@ public class StartEc2Builder extends Builder {
 		PrintStream logger = listener.getLogger();
 		try {
 			for (SlaveTemplate template : templates) {
-				template.parent = EC2Cloud.get(); //TODO: allow user to select which cloud service to use
-				EC2Slave newMachine = template.provision(listener);
-				newMachines.add(newMachine);
+				int count = getCountFromTags(template.getTags(), logger);
+				for(int i = 0; i < count; i++) {
+					template.parent = EC2Cloud.get(); //TODO: allow user to select which cloud service to use
+					EC2Slave newMachine = template.provision(listener);
+					newMachines.add(newMachine);
 
-				logger.println("Created machine " + newMachine.getInstanceId());
+					logger.println("Created machine " + newMachine.getInstanceId());
+				}
 			}
 		} catch(Exception e) {
+			//TODO: If we get throttled on a request minute, sleep and then retry. I don't know what the limit is or what exception is thrown
 			listener.error("Failed to start a machine. Attempting to terminate.");
 			e.printStackTrace(logger);
 
@@ -74,6 +78,32 @@ public class StartEc2Builder extends Builder {
 				break;
 			}
 		}
+	}
+
+	/**
+	 * Allow the user to define how many machines to create of each instance by using the Tags.
+	 * @param tags
+	 * @param logger
+	 * @return Always 1 or more.
+	 */
+	private int getCountFromTags(Iterable<EC2Tag> tags, PrintStream logger) {
+		int count = 1;
+		for (EC2Tag tag : tags) {
+			if("count".equalsIgnoreCase(tag.getName())) {
+				try {
+					String value = tag.getValue();
+					logger.println("Found '" + tag.getName() + "' going to make " + value + " instances of image.");
+					int temp = Integer.parseInt(value);
+					if(temp > 1) {
+						count = temp;
+					}
+				} catch (java.lang.NumberFormatException ignore) {
+					//count isn't a valid number!
+					logger.println("'" + tag.getName() + "' must be a valid integer to use it to define how many instances to create.");
+				}
+			}
+		}
+		return count <= 0 ? 1 : count; //just double check that we're not returning an value less than 1
 	}
 
 	private void waitForAllMachinesSsh(List<EC2Slave> newMachines, PrintStream logger) throws InterruptedException {
